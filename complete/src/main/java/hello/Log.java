04a0e1800c6aea2ad8ce4c06bf9d956926c1e9be
Jacek Musiał <github@musi.al>
2018-03-26 22:40:34 +0200

    Add class Log
    Allows us to create table log containing our logs :computer:

---
+++ complete/src/main/java/hello/Log.java
@@ -0,0 +1,33 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String header;
+
+    private String data;
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
